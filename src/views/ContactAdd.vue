f41f731e39ab4758de1c4cdf6859ef7a6b2e4026
quocThanh <2002nguyenquocthanh@gmail.com>
2024-03-02 15:53:17 +0700

    Them mot lien he moi

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="page">
+        <h4>Thêm liên hệ mới</h4>
+        <ContactForm 
+            :contact="{}"
+            @submit:contact="createContact"
+        />
+        <p>{{ message }}</p>
+    </div>
+</template>
+<script>
+import ContactService from '@/services/contact.service';
+import ContactForm from '@/components/ContactForm.vue';
+export default {
+    components: {
+        ContactForm,
+    },
+    data(){
+        return {
+            message: "",
+        };
+    },
+    methods: {
+        async createContact(data){
+            try{
+                await ContactService.create(data);
+                this.message="Liên hệ được thêm thành công.";
+            }catch (error){
+                console.log(error);
+            }
+        }
+    },
+    created(){
+        this.message = "";
+    },
+};
+</script>
